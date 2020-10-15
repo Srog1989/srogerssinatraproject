8d0f8e7436dad478f2d7a0e98f2060785fa95799
Srog1989 <s.rogers0715@yahoo.com>
2020-10-14 20:33:33 -0600

    built views/routes and spec.md

--- app/controllers/medicines_controller.rb
+++ app/controllers/medicines_controller.rb
@@ -1,21 +1,35 @@
 class MedicinesController < ApplicationController
 
 
-    get '/medicnes' do
-        erb :index
-    end
-
-    get '/medicines/new' do
-        @medicne = Medicne.new
-        erb :"medicine/new"
-    end
-
-    post '/medicines' do
-        @medicine = Medicine.create(params)
-        redirect "/medicines/#{@medicine.id}"
-      end
-
-
+  
+ # GET: /medicines
+ get "/medicines" do
+    erb :"/medicines/index.html"
+  end
+  # GET: /medicines/new
+  get "/medicines/new" do
+    erb :"/medicines/new.html"
+  end
+  # POST: /medicines
+  post "/medicines" do
+    redirect "/medicines"
+  end
+  # GET: /medicines/5
+  get "/medicines/:id" do
+    erb :"/umedicines/show.html"
+  end
+  # GET: /medicines/5/edit
+  get "/medicines/:id/edit" do
+    erb :"/medicines/edit.html"
+  end
+  # PATCH: /medicines/5
+  patch "/medicines/:id" do
+    redirect "/medicines/:id"
+  end
+  # DELETE: /medicines/5/delete
+  delete "/medicines/:id/delete" do
+    redirect "/medicines"
+  end
 
     
 end
